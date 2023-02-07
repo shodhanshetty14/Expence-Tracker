bf0c609f2d8903b4baa8c131cbd2569fd866decd
Shodhan Shetty <Shodhanshetty144@gmail.com>
2023-02-07 23:03:11 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from datetime import datetime as dt
+
+
+# print(dt.now())
+
+
+
+
+
+# f1 = open('spendings.txt', 'a')
+
+# s = "Hello I'm Shodhan Shetty.\nThis is my 1st testing\n"
+# f1.write(s)
+# f1.close()
